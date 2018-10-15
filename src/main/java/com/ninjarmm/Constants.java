0d65971dfe2af08018c93594a8eceb4f502f14ba
darthmiguel <miguelpatriciom@gmail.com>
2018-10-15 00:02:04 -0500

    add Constants class

---
+++ src/main/java/com/ninjarmm/Constants.java
@@ -0,0 +1,9 @@
+package com.ninjarmm;
+
+public class Constants {
+
+  public final static String FORBIDDEN_MESSAGE = "The user is only allowed to manage its own data";
+
+  public final static int SERVICE_TYPE_DEVICE = 1;
+
+}
